f4226e7ca876a795cff272b746410dfc5358d918
manuelarc06 <mrodriguezcardenas@byupathway.edu>
2026-07-17 22:54:50 -0500

    adding part1 project

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ router.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+router.use('/pets', require('./pets'));
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const dns = require("node:dns/promises");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const express = require("express");
+
+const mongodb = require('./data/database.js');
+
 const app = express();
 
 app.use('/', require('./routes'));
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => { console.log(`Running on port ${port}`) });
+mongodb.initDb((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => { console.log(`Database is listening and node running on port ${port}`) });
+    }
+});
