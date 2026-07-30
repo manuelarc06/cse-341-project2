43607da981bee9598732aae5010cc7661f7f3ad3
manuelarc06 <mrodriguezcardenas@byupathway.edu>
2026-07-29 23:08:43 -0500

    server changes

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,9 @@ router.get('/login', passport.authenticate('github'), (req, res) => { });
 router.get('/logout', function (req, res, next) {
     req.logout(function (err) {
         if (err) { return next(err); }
-        res.redirect('/');
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
     });
 });
 
